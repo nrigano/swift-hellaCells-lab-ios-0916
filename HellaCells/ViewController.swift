2ed892fbde6f2c1339b991b54c071249bbb003e8
nrigano <nrigano@gmail.com>
2016-12-22 15:49:04 -0500

    Reference - in progress

--- HellaCells/ViewController.swift
+++ HellaCells/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.height)
+        let tableView = UITableView(frame: frame)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "hellaCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+     
+        self.view.addSubview(tableView)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let hellaCell = tableView.dequeueReusableCell(withIdentifier: "hellaCell", for: indexPath)
+        hellaCell.textLabel?.text = String(indexPath.row + 1)
+        return hellaCell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row + 1)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +42,6 @@ class ViewController: UIViewController {
 
 }
 
+
+//Custom Cell lab
+//create and push to new view controller.  predicated on idea of being a navigation controller
